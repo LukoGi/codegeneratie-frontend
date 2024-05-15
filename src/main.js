80e29d4021008515eb74e031ba6ea9b0c8fa7f29
Gecko <kian.steffes@gmail.com>
2024-05-15 13:21:24 +0200

    added Pinia + Axios packages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-// import { createPinia } from 'pinia'
-// import axios from 'axios'
+import { createPinia } from 'pinia'
+import axios from 'axios'
 
 import App from './App.vue'
 import router from './router'
@@ -9,7 +9,7 @@ import './assets/main.css'
 
 const app = createApp(App)
 
-// app.use(createPinia())
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
