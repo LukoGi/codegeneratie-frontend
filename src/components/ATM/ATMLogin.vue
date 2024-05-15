2a9ba253c248e6c985736e0a438c0e595d55acb6
Gecko <kian.steffes@gmail.com>
2024-05-15 15:48:42 +0200

    ATMLogin page

---
+++ src/components/ATM/ATMLogin.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="login-container d-flex justify-content-center align-items-center" style="height: 80vh;">
+    <form class="p-3 card col-md-3" @submit.prevent="login">
+      <h1>Insert Card</h1>
+
+      <div class="form-group mb-3">
+        <label for="cardname">Name on Card</label>
+        <input
+          type="text"
+          class="form-control"
+          id="cardname"
+          v-model="cardname"
+          required
+        />
+        <small class="text-muted">Full name as displayed on card</small>
+      </div>
+      <div class="form-group mb-3">
+        <label for="iban">IBAN</label>
+        <input
+          type="text"
+          class="form-control"
+          id="iban"
+          v-model="iban"
+          required
+        />
+      </div>
+      <div class="form-group mb-3">
+        <label for="pin">PIN</label>
+        <input
+          type="password"
+          class="form-control"
+          id="pin"
+          v-model="pin"
+          required
+        />
+      </div>
+      <button class="btn btn-primary" type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script></script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../components/Home.vue';
 
+import ATMLogin from '../components/ATM/ATMLogin.vue';
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', component: Home },
+    { path: '/atm/login', component: ATMLogin }
   ]
 })
 
