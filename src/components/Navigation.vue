e4b0a750c963f6399465cc0d10653b154eede8f4
Gecko <kian.steffes@gmail.com>
2024-05-15 15:47:01 +0200

    styled navigation + custom bootstrap

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,13 +1,20 @@
 <template>
-  <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-    <div class="container-fluid">
-      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+  <nav class="navbar navbar-expand-md mb-4">
+    <div class="container-fluid d-flex justify-content-between">
+      <div>
+        <a class="navbar-brand fw-bold" href="#" style="color: #48877a;">Bank Name</a>
+      </div>
+
+      <ul class="navbar-nav mb-2 mb-md-0">
         <li class="nav-item">
-          <router-link to="/" class="nav-link" active-class="active"
-            >Home</router-link
-          >
+          <router-link to="/" class="nav-link" active-class="active">Home</router-link>
         </li>
       </ul>
+
+      <div>
+        <button class="btn btn-primary me-2">Login</button>
+        <button class="btn btn-secondary">Open Account</button>
+      </div>
     </div>
   </nav>
 </template>
@@ -15,4 +22,8 @@
 <script>
 </script>
 
-<style></style>
+<style>
+.navbar {
+  border-bottom: 1px solid #e5e5e5;
+}
+</style>
