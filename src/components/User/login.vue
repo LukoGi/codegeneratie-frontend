390b87f2cbe28693bef34f6ac3d964b40fd36425
Umit3001 <umit30015@gmail.com>
2024-05-27 21:48:32 +0200

    added login auth

--- src/components/User/login.vue
+++ src/components/User/login.vue
@@ -44,20 +44,39 @@ export default {
   },
   methods: {
     login() {
-      axios.post('http://localhost:8080/login', {
+      axios.post('http://localhost:8080/users/login', {
         username: this.username,
         password: this.password
       })
       .then(response => {
         console.log(response.data);
+        
         localStorage.setItem('token', response.data.token);
-        this.$router.push({ path: '/usermenu' });
+        this.getUserInfo();
         
       })
       .catch(error => {
         console.log('Error:', error);
         console.log('Error message:', error.response.data);
          this.errorMessage = error.response.data;
+      });
+    },
+    getUserInfo() {
+      const token = localStorage.getItem('token');
+      axios.get('http://localhost:8080/users/userinfo', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        if (response.data.is_approved) {
+          this.$router.push({ path: '/usermenu' });
+        } else {
+          this.$router.push({ path: '/notapproved' });
+        }
+      })
+      .catch(error => {
+        console.log('Error:', error);
       });
     }
   }
