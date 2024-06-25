76e65af0360c488e44a462c2864e5310116aa0da
LukoGi <lukopecco@gmail.com>
2024-06-25 21:45:56 +0200

    Security And Delete Admin Filters

--- src/components/Admin/TransactionsOverview.vue
+++ src/components/Admin/TransactionsOverview.vue
@@ -2,37 +2,6 @@
   <div class="all-transactions-view">
     <AdminSideNav />
     <div class="container">
-      <div class="card mt-3">
-        <div class="card-body">
-          <h5 class="card-title">Filter Transactions</h5>
-          <div class="row">
-            <div class="col-md-6">
-              <label for="start-date" class="form-label">Start Date</label>
-              <input id="start-date" type="date" class="form-control mb-2" v-model="startDate" placeholder="Start Date" />
-            </div>
-            <div class="col-md-6">
-              <label for="end-date" class="form-label">End Date</label>
-              <input id="end-date" type="date" class="form-control mb-2" v-model="endDate" placeholder="End Date" />
-            </div>
-            <div class="col-md-6">
-              <label for="min-amount" class="form-label">Min Amount</label>
-              <input id="min-amount" type="number" class="form-control mb-2" v-model="minAmount" placeholder="Min Amount" />
-            </div>
-            <div class="col-md-6">
-              <label for="max-amount" class="form-label">Max Amount</label>
-              <input id="max-amount" type="number" class="form-control mb-2" v-model="maxAmount" placeholder="Max Amount" />
-            </div>
-            <div class="col-md-6">
-              <label for="iban" class="form-label">IBAN</label>
-              <input id="iban" type="text" class="form-control mb-2" v-model="iban" placeholder="IBAN" />
-            </div>
-            <div class="col-md-6 d-flex align-items-end">
-              <button class="btn btn-primary mb-2" @click="fetchTransactions">Apply Filters</button>
-            </div>
-          </div>
-          <p class="text-muted">Note: The date range is inclusive. Transactions from the start date to the end date will be displayed.</p>
-        </div>
-      </div>
       <h1 class="text-center mt-5">All Transactions</h1>
       <table class="table table-striped mt-3">
         <thead>
@@ -60,6 +29,7 @@
   </div>
 </template>
 
+
 <script>
 import axios from '@/axios-auth.js';
 import AdminSideNav from './AdminSideNav.vue';
@@ -71,11 +41,6 @@ export default {
   data() {
     return {
       transactions: [],
-      startDate: '',
-      endDate: '',
-      minAmount: '',
-      maxAmount: '',
-      iban: '',
       token: localStorage.getItem('token'),
     };
   },
@@ -99,24 +64,12 @@ export default {
           style: 'currency',
           currency: 'EUR',
         });
-
       });
       return transactions;
     },
     async fetchTransactions() {
       try {
-        let url = '/transactions/';
-        if (this.startDate) url += `?startDate=${new Date(this.startDate).toISOString()}`;
-        if (this.endDate) {
-          const endDateInclusive = new Date(this.endDate);
-          endDateInclusive.setHours(23, 59, 59, 999);
-          url += `&endDate=${endDateInclusive.toISOString()}`;
-        }
-        if (this.minAmount) url += `&minAmount=${this.minAmount}`;
-        if (this.maxAmount) url += `&maxAmount=${this.maxAmount}`;
-        if (this.iban) url += `&iban=${this.iban}`;
-
-        const response = await axios.get(url, {
+        const response = await axios.get('/transactions/', {
           headers: {
             Authorization: `Bearer ${this.token}`,
           },
